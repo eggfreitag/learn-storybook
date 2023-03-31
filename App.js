3ce571407c0c2c688949f8182eee9757f6f9df28
Wonhee Kang <eggfreitag@gmail.com>
2023-03-31 17:43:24 +0900

    Storybook을 위한 AppEntryPoint설정 및 적용

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from "expo-constants";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import MasterNav from "./src/MasterNav";
+
+const App = () => {
+  return <MasterNav />;
+};
+
+let AppEntryPoint = App;
+
+if (Constants.expoConfig.extra.storybookEnabled === "true") {
+  AppEntryPoint = require("./.storybook").default;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default AppEntryPoint;
---
+++ src/MasterNav.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MasterNav = () => {
+  return (
+    <View>
+      <Text>MasterNav</Text>
+    </View>
+  );
+};
+
+export default MasterNav;
